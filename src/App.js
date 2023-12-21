709fc5e9e8929966c884b468f706d077e4295c3c
Raj Bhatia <raj2784@hotmail.com>
2023-12-21 21:49:30 +1030

    bg changer with react and tailwing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("white");
+
   return (
-    <div>
-      <h1>test</h1>
-    </div>
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      ></div>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-4 shadow-lg bg-white px-3 py-2 rounded-md">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-7 rounded-md text-white"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-7 rounded-md text-white"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("orange")}
+            className="outline-none px-7 rounded-md text-white"
+            style={{ backgroundColor: "orange" }}
+          >
+            Orange
+          </button>
+          <button
+            onClick={() => setColor("brown")}
+            className="outline-none px-7 rounded-md text-white"
+            style={{ backgroundColor: "brown" }}
+          >
+            Brown
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className="outline-none px-7 rounded-md text-white"
+            style={{ backgroundColor: "black" }}
+          >
+            Black
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-7 rounded-md text-white"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
